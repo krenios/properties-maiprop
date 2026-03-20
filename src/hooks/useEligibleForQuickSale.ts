896ea79a02a0e72c3560011439b654257c6a5638
K R <krenios@outlook.com>
2026-03-20 18:28:33 +0200

    Revert "Fix missing auth/subscription helpers"
    This reverts commit d836de0a77a26641ef701498116237f6f9e5aee6.

--- src/hooks/useEligibleForQuickSale.ts
+++
@@ -1,6 +0,0 @@
-export function useEligibleForQuickSale() {
-  // Minimal implementation: no one is eligible until we wire this to real
-  // subscription/eligibility rules.
-  return { eligible: false };
-}
-
